2e767f77145d0a3ae0446612c0a3180d9503336a
Александр Иванович Самаркин <alexsamarkinru@gmail.com>
2022-02-24 00:00:17 +0300

    Добавьте файлы проекта.

---
+++ Lesson3/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Lesson3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.общееРасписаниеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.самаркинАИToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.самаркинаЕИToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.мудровАСToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.общееРасписаниеToolStripMenuItem,
+            this.самаркинАИToolStripMenuItem,
+            this.самаркинаЕИToolStripMenuItem,
+            this.мудровАСToolStripMenuItem,
+            this.toolStripComboBox1,
+            this.toolStripTextBox1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1001, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // общееРасписаниеToolStripMenuItem
+            // 
+            this.общееРасписаниеToolStripMenuItem.Name = "общееРасписаниеToolStripMenuItem";
+            this.общееРасписаниеToolStripMenuItem.Size = new System.Drawing.Size(126, 23);
+            this.общееРасписаниеToolStripMenuItem.Text = "Общее расписание";
+            this.общееРасписаниеToolStripMenuItem.Click += new System.EventHandler(this.общееРасписаниеToolStripMenuItem_Click);
+            // 
+            // самаркинАИToolStripMenuItem
+            // 
+            this.самаркинАИToolStripMenuItem.Name = "самаркинАИToolStripMenuItem";
+            this.самаркинАИToolStripMenuItem.Size = new System.Drawing.Size(101, 23);
+            this.самаркинАИToolStripMenuItem.Text = "Самаркин А.И.";
+            this.самаркинАИToolStripMenuItem.Click += new System.EventHandler(this.самаркинАИToolStripMenuItem_Click);
+            // 
+            // самаркинаЕИToolStripMenuItem
+            // 
+            this.самаркинаЕИToolStripMenuItem.Name = "самаркинаЕИToolStripMenuItem";
+            this.самаркинаЕИToolStripMenuItem.Size = new System.Drawing.Size(105, 23);
+            this.самаркинаЕИToolStripMenuItem.Text = "Самаркина Е.И.";
+            this.самаркинаЕИToolStripMenuItem.Click += new System.EventHandler(this.самаркинаЕИToolStripMenuItem_Click);
+            // 
+            // мудровАСToolStripMenuItem
+            // 
+            this.мудровАСToolStripMenuItem.Name = "мудровАСToolStripMenuItem";
+            this.мудровАСToolStripMenuItem.Size = new System.Drawing.Size(87, 23);
+            this.мудровАСToolStripMenuItem.Text = "Мудров А.С.";
+            this.мудровАСToolStripMenuItem.Click += new System.EventHandler(this.мудровАСToolStripMenuItem_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyUp);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 27);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1001, 614);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("http://www.yandex.ru", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 641);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Расписание";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem общееРасписаниеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem самаркинАИToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem самаркинаЕИToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem мудровАСToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Lesson3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // список адресов
+            SList.Add(@"http://rasp.pskgu.ru/PrepsRasp.html");
+            SList.Add(@"http://rasp.pskgu.ru/preps/samarkinai.html");
+            SList.Add(@"http://rasp.pskgu.ru/preps/samarkinaei.html");
+            SList.Add(@"http://rasp.pskgu.ru/preps/mudrovas.html");
+
+            // заполняем адресами combobox
+            toolStripComboBox1.Items.AddRange(SList.ToArray());
+            // выбираем из этого списка первый (элемент с номером 0)
+            toolStripComboBox1.SelectedIndex = 0;
+            // при этом (см. далее) происходит СОБЫТИЕ - SelectedIndexChanged
+        }
+
+        public List<string> SList { get; set; } = new List<string>();
+
+        private void общееРасписаниеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Url = new Uri(SList[0]);
+        }
+
+        private void самаркинАИToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Url = new Uri(SList[1]); // Самаркин
+        }
+
+        private void самаркинаЕИToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Url = new Uri(SList[2]); // Самаркина Е.И.
+        }
+
+        private void мудровАСToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Url = new Uri(SList[3]); // Мудров
+        }
+
+        private void toolStripTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                webBrowser1.Url = new Uri(toolStripTextBox1.Text);
+            }
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(toolStripComboBox1.Text);
+        }
+    }
+}
